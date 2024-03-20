59356f92932f7dd9560f1adaaddd0950b12cf0ce
Seif Eldeen Essam <seifeldeenessam01@gmail.com>
2024-03-20 16:47:15 +0200

    Updated app icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,8 +4,9 @@ export default function Document() {
 	return (
 		<Html>
 			<Head>
-				<title>Jobbox - Job Portal HTML Template</title>
-				<link rel="icon" href="/favicon.ico" />
+				<title>JobBox</title>
+				<link rel="icon" href="/favicon.ico" sizes="any" />
+				<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
 				<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" />
