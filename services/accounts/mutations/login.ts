d2e9e79fcd53911f6ad1e646ab23fbf6030793e9
Seif Eldeen Essam <seifeldeenessam01@gmail.com>
2024-03-23 19:06:40 +0200

    Updated `account` service login mutation

--- services/accounts/mutations/login.ts
+++ services/accounts/mutations/login.ts
@@ -6,22 +6,20 @@ import { LoginPayload, LoginResponse } from '../types';
 
 type Props = QueryParams & { payload: LoginPayload };
 
-const login = async (props: Props) => {
-	const api = createAxiosInstance({ req: props.req, res: props.res });
-
-	try {
-		const response = await api.post(Endpoints.ACCOUNT_LOGIN, props.payload);
-		return response.data;
-	} catch (error: any) {
-		throw Error(error.message, { cause: 'Error logging in user' });
-	}
-};
-
 // TODO: Handle `any` type (should be the error type)
 export const useLogin = () => {
 	const mutation = useMutation<LoginResponse, any, Props>({
-		mutationFn: login,
-		mutationKey: [Endpoints.ACCOUNT_LOGIN]
+		mutationKey: [Endpoints.ACCOUNT_LOGIN],
+		mutationFn: async (props) => {
+			const api = createAxiosInstance({ req: props.req, res: props.res });
+
+			try {
+				const response = await api.post(Endpoints.ACCOUNT_LOGIN, props.payload);
+				return response.data;
+			} catch (error: any) {
+				throw Error(error.message, { cause: 'Error logging in user' });
+			}
+		}
 	});
 
 	return mutation;
