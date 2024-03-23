0583574c91ac59d23c76bab2f6f51f9bbd453ac9
Seif Eldeen Essam <seifeldeenessam01@gmail.com>
2024-03-23 19:12:51 +0200

    Updated `Session` type

--- types/session.ts
+++ types/session.ts
@@ -1,16 +1,4 @@
 export type Session = {
 	refresh: string;
 	access: string;
-	user: SessionUser;
-};
-
-export type SessionUser = {
-	id: number;
-	first_name: string;
-	last_name: string;
-	username: string;
-	email: string;
-	role: string;
-	avatar: null | string;
-	is_active: boolean;
 };
