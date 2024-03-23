c5e1c238625eccf9a7d13f2a7c82fd5e5d0a707a
Seif Eldeen Essam <seifeldeenessam01@gmail.com>
2024-03-23 19:06:46 +0200

    Updated `account` service register mutation

--- services/accounts/mutations/register.ts
+++ services/accounts/mutations/register.ts
@@ -6,22 +6,20 @@ import { RegisterPayload, RegisterResponse } from '../types';
 
 type Props = QueryParams & { payload: RegisterPayload };
 
-const register = async (props: Props) => {
-	const api = createAxiosInstance({ req: props.req, res: props.res });
-
-	try {
-		const response = await api.post(Endpoints.ACCOUNT_REGISTER, props.payload);
-		return response.data;
-	} catch (error: any) {
-		throw Error(error.message, { cause: 'Error registering user' });
-	}
-};
-
 // TODO: Handle `any` type (should be the error type)
 export const useRegister = () => {
 	const mutation = useMutation<RegisterResponse, any, Props>({
-		mutationFn: register,
-		mutationKey: [Endpoints.ACCOUNT_REGISTER]
+		mutationKey: [Endpoints.ACCOUNT_REGISTER],
+		mutationFn: async (props) => {
+			const api = createAxiosInstance({ req: props.req, res: props.res });
+
+			try {
+				const response = await api.post(Endpoints.ACCOUNT_REGISTER, props.payload);
+				return response.data;
+			} catch (error: any) {
+				throw Error(error.message, { cause: 'Error registering user' });
+			}
+		}
 	});
 
 	return mutation;
