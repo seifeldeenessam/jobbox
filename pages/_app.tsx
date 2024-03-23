8bedb1e7a7511c1c95d07ddac15eefe6191e538a
Seif Eldeen Essam <seifeldeenessam01@gmail.com>
2024-03-23 17:21:45 +0200

    Initialized `QueryClient`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,24 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AppProps } from 'next/app';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Layout from '../components/common/Layout';
 import '../public/assets/css/style.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
+	const [queryClient] = useState(() => new QueryClient({ defaultOptions: { queries: { refetchOnWindowFocus: false } } }));
+
 	useEffect(() => {
 		require('../public/assets/js/bootstrap.bundle.min.js');
 	}, []);
 
+	// TODO: Handle hydration (<Hydrate></Hydrate> component)
+
 	return (
-		<Layout>
-			<Component {...pageProps} />;
-		</Layout>
+		<QueryClientProvider client={queryClient}>
+			<Layout>
+				<Component {...pageProps} />;
+			</Layout>
+		</QueryClientProvider>
 	);
 }
 
