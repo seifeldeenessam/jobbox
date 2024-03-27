4f549e12da26859ef80b76b7af1c270f3733ee4a
Seif Eldeen Essam <seifeldeenessam01@gmail.com>
2024-03-27 17:26:06 +0200

    Fixed `getFormStatus` pending state

--- utilities/forms.ts
+++ utilities/forms.ts
@@ -2,7 +2,7 @@ import { UseMutationResult } from '@tanstack/react-query';
 
 export const getFormStatus = (status: UseMutationResult['status'], initialLabel: string, errorLabel: string, successLabel: string) => {
 	if (status === 'idle') return { className: 'btn-brand-1', disabled: false, label: initialLabel };
-	if (status === 'pending') return { className: 'btn-brand-1', disabled: true, label: initialLabel };
+	if (status === 'pending') return { className: 'btn-brand-1', disabled: true, label: 'Loading...' };
 	if (status === 'error') return { className: 'btn-brand-error', disabled: false, label: errorLabel };
 	return { className: 'btn-brand-success', disabled: false, label: successLabel };
 };
