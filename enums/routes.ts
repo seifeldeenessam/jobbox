1df9725ebed266d365dc450c671ce090526b1cff
Seif Eldeen Essam <seifeldeenessam01@gmail.com>
2024-03-27 17:01:44 +0200

    Updated `Routes` enum

--- enums/routes.ts
+++ enums/routes.ts
@@ -1,8 +1,30 @@
 export enum Routes {
 	ROOT = '/',
 
+	// Account
 	ACCOUNT_LOGIN = '/account/login',
 	ACCOUNT_REGISTER = '/account/register',
 	ACCOUNT_RESET_PASSWORD = '/account/reset-password',
-	ACCOUNT_PROFILE = '/account/profile'
+	ACCOUNT_PROFILE = '/account/profile',
+
+	// Jobs
+	JOBS = '/jobs',
+
+	// Recruiters
+	RECRUITERS = '/recruiters',
+
+	// Candidates
+	CANDIDATES = '/candidates',
+
+	// Blog
+	BLOG = '/blog',
+
+	// Contact
+	CONTACT = '/contact',
+
+	// About
+	ABOUT = '/about',
+
+	// Pricing
+	PRICING = '/pricing'
 }
