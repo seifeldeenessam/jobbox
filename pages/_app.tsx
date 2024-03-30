a415a39ab759126224d84d9bd84df3013607d631
Seif Eldeen Essam <seifeldeenessam01@gmail.com>
2024-03-30 16:10:39 +0200

    Handled global stylesheets imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,23 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AppProps } from 'next/app';
 import { useEffect, useState } from 'react';
-import '../public/assets/css/style.css';
-import '../public/dashboard-assets/css/style.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, router }: AppProps) {
 	const [queryClient] = useState(() => new QueryClient({ defaultOptions: { queries: { refetchOnWindowFocus: false } } }));
 
 	useEffect(() => {
 		require('../public/scripts/bootstrap.bundle.min.js');
 	}, []);
+
+	useEffect(() => {
+		const isDashboardPage = router.pathname.includes('profile');
+
+		if (isDashboardPage) {
+			require('../public/dashboard-assets/css/style.css');
+		} else {
+			require('../public/assets/css/style.css');
+		}
+	}, [router.pathname]);
 
 	// TODO: Handle hydration (<Hydrate></Hydrate> component)
 
