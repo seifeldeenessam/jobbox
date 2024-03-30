2693c46c35fecc53f260eaf0d6b5c8da8dd9d8be
Seif Eldeen Essam <seifeldeenessam01@gmail.com>
2024-03-30 16:10:18 +0200

    Fixed `stylesheet` link warning

--- components/common/ProfileLayout/partials/PageHead/index.tsx
+++ components/common/ProfileLayout/partials/PageHead/index.tsx
@@ -7,10 +7,9 @@ type Props = {
 const PageHead = ({ headTitle }: Props) => {
 	return (
 		<Head>
-			<title>{headTitle ? headTitle : 'Jobbox Dashboard - Job Portal HTML Template'}</title>
+			<title>{headTitle ? headTitle : 'JobBox Dashboard - Job Portal HTML Template'}</title>
 			<link rel="shortcut icon" href="//dashboard-assets/imgs/template/favicon.svg" />
 			<link rel="preconnect" href="https://fonts.bunny.net" />
-			<link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
 		</Head>
 	);
 };
